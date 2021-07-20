c21348ff22b4c62ec50ae3fe0378f2e35267a02c
mferrara625 <mferrara401@yahoo.com>
2021-07-20 11:57:07 -0400

    updated gameplay, improved visuals

--- src/com/company/House.java
+++ src/com/company/House.java
@@ -4,6 +4,9 @@ public class House {
     String window1 = "________";
     String window2 = "________";
     String window3 = "________";
+    String topOf1 = "        ";
+    String topOf2 = "        ";
+    String topOf3 = "        ";
 
     public House(){
 
@@ -12,14 +15,17 @@ public class House {
         window1 = "________";
         window2 = "________";
         window3 = "________";
+        topOf1 = "        ";
+        topOf2 = "        ";
+        topOf3 = "        ";
     }
     public String toString(){
         return  "                                              ________________________________________\n" +
                 "                                             /\\uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu\\\n" +
                 "                                            /oo\\uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu\\ \n" +
                 "                                           /____\\_______________________________________\\\n" +
-                "                                           | oooo|  __________  __________  __________  |                    \n" +
-                "                                           |ooooo|  |        |  |        |  |        |  |\n" +
+                "                                           | oooo|  __________  __________  __________  |\n" +
+                "                                           |ooooo|  |"+topOf1+"|  |"+topOf2+"|  |"+topOf3+"|  |\n" +
                 "                                           |ooooo|  |"+ window1 +"|  |"+ window2 +"|  |"+ window3 +"|  |\n" +
                 "                                           |ooooo|      __                __            |\n" +
                 "                                           |ooooo|     | .|              | .|           |\n" +
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,36 +9,86 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         House house = new House();
         Player player = new Player();
+        System.out.println(house);
         for (int i = 0; true; i++) {
-            System.out.println(house);
+//            System.out.println(house);
             System.out.println("\nPick a window to Attack (1 - 3) : ");
             int input = scan.nextInt();
             player.takeTurn();
-            if(player.currentPosition == 1)
-                house.window1 = "(︡• _⦣•︠)";
-            else if (player.currentPosition == 2)
-                house.window2 = "(︡• _⦣•︠)";
-            else
-                house.window3 = "(︡• _⦣•︠)";
-            System.out.println(house);
-            if (input == player.currentPosition) {
-                System.out.println("HIT!!! +25 pts");
-                player.score += 25;
-                player.hits++;
-            } else {
-                System.out.println("MISS :(");
-                player.lives--;
+            if (player.currentPosition == 1) {
+                if (input == 1) {
+                    house.topOf1 = "︻┳═一** ";
+                    house.window1 = "(︡> _⦣<︠)";
+                } else if (input == 2) {
+                    house.window1 = "(︡• _⦣•︠)";
+                    house.window2 = "︻┳═一**_";
+                } else if (input == 3){
+                    house.window1 = "(︡• _⦣•︠)";
+                    house.window3 = "︻┳═一**_";
+                }
             }
-            house.reset();
-            if ( player.lives == 0 || player.hits == 3)
-                break;
+                else if (player.currentPosition == 2) {
+                    if (input == 2){
+                        house.topOf2 = "︻┳═一** ";
+                        house.window2 = "(︡> _⦣<︠)";
+                    }
+                    else if (input == 1){
+                        house.window2 = "(︡• _⦣•︠)";
+                        house.window1 = "︻┳═一**_";
+                    } else if (input == 3){
+                        house.window2 = "(︡• _⦣•︠)";
+                        house.window3 = "︻┳═一**_";
+                    }
+                } else if (player.currentPosition == 3) {
+                    if (input == 3) {
+                        house.topOf3 = "︻┳═一** ";
+                        house.window3 = "(︡> _⦣<︠)";
+                    }
+                    else if (input == 2) {
+                        house.window3 = "(︡• _⦣•︠)";
+                        house.window2 = "︻┳═一**_";
+                    }
+                    else if (input == 1){
+                        house.window3 = "(︡• _⦣•︠)";
+                        house.window1 = "︻┳═一**_";
+                    }
+                }
+                System.out.println(house);
+                if (input == player.currentPosition) {
+                    System.out.println("HIT!!! +25 pts");
+                    player.score += 25;
+                    player.hits++;
+                } else {
+                    System.out.println("MISS :(");
+                    player.lives--;
+                }
+                if (player.lives == 0 || player.hits == 3)
+                    break;
+                house.reset();
+            }
+            if (player.hits >= 3) {
+                System.out.println("Target Eliminated, you WIN!!!  +200 pts");
+                player.score += 200;
+                if(player.currentPosition == 1){
+                    house.reset();
+                    house.window1 = "(︡╥ _⦣╥︠)";
+                }
+                 else if(player.currentPosition == 2){
+                    house.reset();
+                    house.window2 = "(︡╥ _⦣╥︠)";
+                }
+                else if(player.currentPosition == 3){
+                    house.reset();
+                    house.window3 = "(︡╥ _⦣╥︠)";
+                }
+
+            } else {
+                System.out.println("Game over");
+                house.reset();
+                house.window2 = "(︡• \uD83D\uDC45•︠).";
+            }
+            System.out.println(house);
+            System.out.println("Final Score: " + player.score);
         }
-        if (player.hits >= 3) {
-            System.out.println("Target Eliminated, you WIN!!!  +200 pts");
-            player.score += 200;
-        } else {
-            System.out.println("Game over");
-        }
-        System.out.println("Final Score: " + player.score);
     }
-}
+
